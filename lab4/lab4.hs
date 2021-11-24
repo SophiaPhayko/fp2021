8e1161a254916822548e4fbed8f0374cdd691901
Sophia Phayko <sophiaphayko@gmail.com>
2021-11-24 13:35:04 +0200

    вщту

--- lab4/lab4.hs
+++ lab4/lab4.hs
@@ -56,16 +56,6 @@ getFigures str array
   | str == "Text" = getLabels array
   | otherwise = []
 
---Тестування
---getFigures "Label" [(Circle 1 2 3),(Rect 1 1 4 4),(Triangle 1 1 4 4 9 9),(Rectangle 1 1 4 5), (Label 0 0 Consolas "hello")]
---[Label 0.0 0.0 Consolas "hello"]
---getFigures "Rectangle" [(Circle 1 2 3),(Rect 1 1 4 4),(Triangle 1 1 4 4 9 9),(Rectangle 1 1 4 5), (Label 0 0 Consolas "hello")]
---[Rect 1.0 1.0 4.0 4.0,Rect 1.0 1.0 4.0 5.0]
---getFigures "Triangle" [(Circle 1 2 3),(Rect 1 1 4 4),(Triangle 1 1 4 4 9 9),(Rectangle 1 1 4 5), (Label 0 0 Consolas "hello")]
---[Triangle 1.0 1.0 4.0 4.0 9.0 9.0]
---getFigures "Circle" [(Circle 1 2 3),(Rect 1 1 4 4),(Triangle 1 1 4 4 9 9),(Rectangle 1 1 4 5), (Label 0 0 Consolas "hello")]
---[Circle 1.0 2.0 3.0]
-
 --Додаткове завдання
 --перемiщення фiгури на вказаний вектор.
 move :: Figure -> Int -> Int -> Figure
@@ -73,16 +63,6 @@ move (Rect x1 y1 x2 y2) v1 v2 = Rect (x1 + v1) (y1 + v2) (x2 + v1) (y2 + v2)
 move (Circle x y r) v1 v2 = Circle (x + v1) (y + v2) r
 move (Text x y f s) v1 v2 = Text (x + v1) (y + v2) f s
 move (Triangle x1 y1 x2 y2 x3 y3) v1 v2 = Triangle (x1 + v1) (y1 + v2) (x2 + v1) (y2 + v2) (x3 + v1) (y3 + v2)
-
---Тестування
---move (Triangle 1 1 4 4 9 9) 2 2
---Triangle 3 3 6 6 11 11
---move (Circle 1 2 3) 3 1
---Circle 4 3 3
---move (Rectangle 1 1 4 4) 5 6
---Rectangle 6 7 9 10
---move (Label 2 4 Consolas "hello") (-1) (-2)
---Label 1 2 Consolas "hello"
 
 --Висновок: під час даної лабораторної робооти я дізналась про нові типи та 
 -- класи типів, нові зазнання використала на практиці.
